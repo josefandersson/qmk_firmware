147d90d5e964e562df0d3afa75072729a3f86502
Josef Andersson <josefandersson@users.noreply.github.com>
2022-12-17 21:15:59 +0100

    Working key mapping

---
+++ keyboards/andersson/alpha/config.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "config_common.h"
+/*
+Copyright 2022 Thomas Baart <thomas@splitkb.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+/* key matrix size */
+/* Rows are doubled up */
+#define MATRIX_ROWS 10
+#define MATRIX_COLS 7
+
+// wiring
+#define MATRIX_ROW_PINS { D4, C6, D7, E6, B4 }
+#define MATRIX_COL_PINS { B5, B6, B2, B3, B1, F7, F6 }
+
+/* COL2ROW, ROW2COL*/
+#define DIODE_DIRECTION COL2ROW
+
+/* define if matrix has ghost (lacks anti-ghosting diodes) */
+//#define MATRIX_HAS_GHOST
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE        5
+
+#define USE_I2C
+// #define LED_CAPS_LOCK_PIN D1
+
+// #ifdef OLED_ENABLE
+// #    define OLED_DISPLAY_128X64
+// #    define SPLIT_OLED_ENABLE
+// #endif
+
+// /* RGB light support */
+#ifdef RGBLIGHT_ENABLE
+    #define RGB_DI_PIN F4
+    #define RGB_MATRIX_LED_COUNT 1 // Number of LEDs
+    #define RGB_MATRIX_SPLIT { 0, 1 }
+#endif
---
+++ keyboards/andersson/alpha/rules.mk
@@ -0,0 +1,10 @@
+MCU = atmega32u4
+BOOTLOADER = caterina
+
+# OLED_ENABLE = yes
+# OLED_DRIVER = SSD1306     # Enables the use of OLED displays
+EXTRAKEY_ENABLE = yes
+BACKLIGHT_ENABLE = no
+RGBLIGHT_ENABLE = no
+
+SPLIT_KEYBOARD = yes
