095352c75a6e79fbe67f86bf8527b48479c21ef9
Josef Andersson <josefandersson@users.noreply.github.com>
2022-12-20 20:45:16 +0100

    Fix missing keys on raise layer

--- keyboards/andersson/alpha/keymaps/default/keymap.c
+++ keyboards/andersson/alpha/keymaps/default/keymap.c
@@ -113,11 +113,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // |     |  ´  |  `  |  #  |  !  |   @   |   |   |  |   |   |  \  | HOME | DARR | END  | PGDN |     |
     //                   |     |     | BKSPC |   |   |  |   |   |     |      |      |
     [_RAISE] = LAYOUT(
-        _______, _______,    _______,     _______, _______,  _______,                                          _______,      _______, _______, _______,  _______, _______,
-        _______, KC_F1,      KC_F2,       KC_F3,   KC_F4,    KC_F5,                                            KC_F6,        KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,
-        _______, SV_DOUBLEQ, KC_S,        KC_D,    KC_F,     SV_PERCENT,                                       SV_PLUS,      KC_LEFT, KC_UP,   KC_RIGHT, KC_PGDN, KC_F12,
-        _______, SV_GRAVE,   SV_BACKTICK, SV_HASH, SV_EXCLM, SV_AT,      _______, _______,   KC_MPRV, KC_MNXT, SV_BACKSLASH, KC_HOME, KC_DOWN, KC_END,   KC_PGDN, _______,
-                                          _______, _______,  KC_BSPC,    _______, _______,   _______, _______, _______,      _______, _______
+        _______, _______,    _______,     _______, _______,  _______,                                               _______,      _______, _______, _______,  _______, _______,
+        _______, KC_F1,      KC_F2,       KC_F3,   KC_F4,     KC_F5,                                                KC_F6,        KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,
+        _______, SV_DOUBLEQ, SV_ASTERIX, SV_PIPE,  SV_DOLLAR, SV_PERCENT,                                           SV_PLUS,      KC_LEFT, KC_UP,   KC_RIGHT, KC_PGDN, KC_F12,
+        _______, SV_GRAVE,   SV_BACKTICK, SV_HASH, SV_EXCLM,  SV_AT,      _______, _______,       KC_MPRV, KC_MNXT, SV_BACKSLASH, KC_HOME, KC_DOWN, KC_END,   KC_PGDN, _______,
+                                          _______, _______,   KC_BSPC,    _______, _______,       _______, _______, _______,      _______, _______
     ),
 };
 // clang-format on
